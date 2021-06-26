ce43985574e57ae8ce6b0343609d22d373fcfbc7
Anik Thomas <aniek.sw@gmail.com>
2021-06-25 20:05:29 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 connect.then(() => {
-
     console.log('Connected correctly to server');
-
     //create method return a promise that resolves to the new document
     Campsite.create({
         name: 'React Lake Campground',
@@ -20,13 +19,25 @@ connect.then(() => {
     //then we console.log that save document,so we expect here to see react lake campground logged as an object
     .then(campsite => {
         console.log(campsite);
-        //next we find and logged the docs instantiated from the campsite model,here we would expect to see
-        //that the react lake campground document is logged as an object that's inside an array.
-        //its only document that we've created from the campsite model so far,so it would be the only item in the array
-        return Campsite.find();
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document'}
+        },{
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite =>{
+        console.log(campsite);
+
+        //Add subDocumentArray
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         //then we delete all the documents from the campsite model & we close the connection
         return Campsite.deleteMany();
     })
